a7716844c0d5be71476db0c5351ee76c628a08b0
Muhannad-09 <muhannadgsgs@gmail.com>
2025-06-25 16:36:29 +0000

    Root user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,7 +1,4 @@
 -- Task: Create MySQL user user_0d_1 with full privileges
--- This script creates the user 'user_0d_1'@'localhost' with password 'user_0d_1_pwd'
--- and grants them all privileges on the server.
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
