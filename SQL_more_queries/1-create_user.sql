b5eb81ea357cb7934ef39ad321845f02e4802619
Muhannad-09 <muhannadgsgs@gmail.com>
2025-06-25 19:55:46 +0000

    1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
--- Task: Create MySQL user user_0d_1 with full privileges
+-- Create user if not exists
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+-- Grant all privileges on all databases and tables
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
