f58a4858dfe2c9379738d8de9caf682dbee5d170
Muhannad-09 <Muhannadgsgs@gmail.com>
2025-06-29 17:00:31 +0000

    SQL Injection...

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,31 +1,23 @@
 #!/usr/bin/python3
 """
-do you remember the previous task? Did you test "Arizona'; TRUNCATE
-TABLE states ; SELECT * FROM states WHERE name = '" as an input?
+Displays all states that match the provided name (safe from SQL injection).
 """
-import MySQLdb
+
 import sys
+import MySQLdb
+
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-
     db = MySQLdb.connect(
         host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
-    cur = db.cursor()
+    cursor = db.cursor()
     query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cur.execute(query, (state_name,))
-    rows = cur.fetchall()
-
-    for row in rows:
+    cursor.execute(query, (sys.argv[4],))
+    for row in cursor.fetchall():
         print(row)
-
-    cur.close()
+    cursor.close()
     db.close()
