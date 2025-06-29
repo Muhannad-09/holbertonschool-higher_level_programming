dd2dd86ece187b4f0c2a606a0deea868bfc73930
Muhannad-09 <Muhannadgsgs@gmail.com>
2025-06-29 16:53:33 +0000

    All cities by state

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,17 +1,37 @@
 #!/usr/bin/python3
-"""Script that takes in the name of a state as an argument and lists
-all cities of that state, using the database hbtn_0e_4_usa"""
+"""Lists all cities of a given state from the database hbtn_0e_4_usa."""
+
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
+    # Extract arguments
+    username, password, db_name, state_name = sys.argv[1:5]
+
+    # Connect to the database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
+
     cur = db.cursor()
-    cur.execute("SELECT cities.name FROM cities \
-    JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s \
-    ORDER BY cities.id", (argv[4],))
+
+    # Secure query with parameterization (SQL injection safe)
+    query = """
+        SELECT cities.name FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+    """
+
+    cur.execute(query, (state_name,))
     rows = cur.fetchall()
-    print(", ".join(city[0] for city in rows))
+
+    # Join city names into comma-separated string
+    print(", ".join([row[0] for row in rows]))
+
     cur.close()
     db.close()
