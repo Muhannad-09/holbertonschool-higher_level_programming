2631f30dd5fd640dcf781fb93bd1583fb09f1679
Muhannad-09 <muhannadgsgs@gmail.com>
2025-07-15 13:23:35 +0000

    Star Wars movies

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,13 @@
+fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+  .then(response => response.json())
+  .then(data => {
+    const movies = data.results;
+    const list = document.getElementById('list_movies');
+
+    movies.forEach(movie => {
+      const item = document.createElement('li');
+      item.textContent = movie.title;
+      list.appendChild(item);
+    });
+  })
+  .catch(error => console.error('Error:', error));
